85f68b2ae4237200724e5beeefca13de810780bc
nevvispro01 <66466744+nevvispro01@users.noreply.github.com>
2025-03-03 15:53:18 +0500

    update

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package ChatLibraryGo
 
 type AuthType int
+type Result int
 
 const (
 	SignIn AuthType = iota
 	SignUp
 )
 
+const (
+	Success Result = iota
+	Error
+)
+
 type AuthSign struct {
 	Name     string   `json:"name"`
 	Password string   `json:"password"`
 	Sign     AuthType `json:"sign"`
+}
+
+type AuthAnswer struct {
+	Result  Result `json:"result"`
+	Message string `json:"message"`
 }
